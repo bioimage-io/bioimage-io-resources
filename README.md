48d64981d16e3d8e6d9a042e61ac4b910492599c
Wei Ouyang <oeway007@gmail.com>
2020-02-24 21:45:57 +0100

    add applications for bio-engine

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ A list of models can be found [here](./src/models.yaml).
 
 ## How to contribute new models?
  1. Fork this repo
- 1. Add your models to the `src/models.yaml` file
+ 1. Add your models to the `src/manifest.model.yaml` file
  1. Run `python src/compile_model_manifest.py` to generate a new `manifest.model.json` with your models
  1. Commit your changes and send us a Pull Request.
--- src/compile_model_manifest.py
+++ src/compile_model_manifest.py
@@ -9,10 +9,16 @@ compiled_models = []
 preserved_keys = ["config_url", "applications", "download_url", "name", "description", "cite", "authors", "documentation", "tags", "covers"]
 assert 'url' not in preserved_keys
 
-model_list_file = Path('src/models.yaml')
-model_list = yaml.safe_load(model_list_file.read_text())
+models_yaml_file = Path('src/manifest.model.yaml')
+models_yaml = yaml.safe_load(models_yaml_file.read_text())
 
-for item in model_list:
+compiled_apps = {}
+for k in models_yaml['applications']:
+    app_url = models_yaml['applications'][k].strip('/').strip('./')
+    app_url = models_yaml['url_root'].strip('/') + '/' + app_url
+    compiled_apps[k] = app_url
+
+for item in models_yaml['models']:
     config_url = item['config_url']
     root_url = '/'.join(config_url.split('/')[:-1])
     response = requests.get(config_url)
@@ -40,9 +46,9 @@ for item in model_list:
             model_info[k] = model_config[k]
             
     compiled_models.append(model_info)
+    compiled_models.sort(key=lambda m: m['name'], reverse=True)
 
-template_to_read = Path('src/manifest_template.json')
-template = json.loads(template_to_read.read_text())
 with open('manifest.model.json', 'wb') as f:
-    template['models'] = compiled_models
-    f.write(json.dumps(template, sort_keys=True, indent=2, separators=(',', ': ')).encode('utf-8'))
+    models_yaml['models'] = compiled_models
+    models_yaml['applications'] = compiled_apps
+    f.write(json.dumps(models_yaml, indent=2, separators=(',', ': ')).encode('utf-8'))
