f548ab89db605922af906e9363dfe0fa6a7c5ebf
Wei Ouyang <oeway007@gmail.com>
2020-06-22 18:11:53 +0200

    Format file

--- src/compile_model_manifest.py
+++ src/compile_model_manifest.py
@@ -31,7 +31,6 @@ models_yaml_file = Path(__file__).parent / "manifest.bioimage.io.yaml"
 models_yaml = yaml.safe_load(models_yaml_file.read_text())
 
 
-
 compiled_apps = []
 apps_names = []
 for item in models_yaml["applications"]:
@@ -44,32 +43,44 @@ for item in models_yaml["applications"]:
     if response.status_code != 200:
         print("Failed to fetch model config from " + app_url)
         continue
-    
-    content = response.content.decode('utf-8')
-    found = re.findall('<config (.*)>(.*)</config>', content, re.DOTALL)[0]
-    if 'json' in found[0]:
+
+    content = response.content.decode("utf-8")
+    found = re.findall("<config (.*)>(.*)</config>", content, re.DOTALL)[0]
+    if "json" in found[0]:
         plugin_config = json.loads(found[1])
-    elif 'yaml' in found[0]:
+    elif "yaml" in found[0]:
         plugin_config = yaml.safe_load(found[1])
     else:
         raise Exception("config not found in " + app_url)
 
     app_config = {"id": item["id"], "type": "application", "source": app_url}
-    fields = ["icon", "name", "version", "api_version", "description", "license", "requirements", "dependencies", "env"]
+    fields = [
+        "icon",
+        "name",
+        "version",
+        "api_version",
+        "description",
+        "license",
+        "requirements",
+        "dependencies",
+        "env",
+    ]
     for f in fields:
         if f in plugin_config:
             app_config[f] = plugin_config[f]
-    tags = plugin_config.get('labels', []) + plugin_config.get('flags', [])
-    app_config['tags'] = tags
-    app_config['covers'] = plugin_config.get('cover', [])
-    app_config['authors'] = plugin_config.get('author', [])
-    assert item["id"] == plugin_config["name"], "Please use the app name (" + plugin_config["name"] + ") as its application id."
+    tags = plugin_config.get("labels", []) + plugin_config.get("flags", [])
+    app_config["tags"] = tags
+    app_config["covers"] = plugin_config.get("cover", [])
+    app_config["authors"] = plugin_config.get("author", [])
+    assert item["id"] == plugin_config["name"], (
+        "Please use the app name (" + plugin_config["name"] + ") as its application id."
+    )
     apps_names.append(plugin_config["name"])
     compiled_apps.append(app_config)
 
 for tp in ["models", "datasets", "notebooks"]:
     for item in models_yaml[tp]:
-        
+
         source = item["source"]
         root_url = "/".join(source.split("/")[:-1])
         response = requests.get(source)
@@ -81,7 +92,7 @@ for tp in ["models", "datasets", "notebooks"]:
 
         # merge item from models.yaml to model config
         model_config.update(item)
-        model_info = {"root_url": root_url, "type": tp[:-1]} # remove `s`
+        model_info = {"root_url": root_url, "type": tp[:-1]}  # remove `s`
         for k in model_config:
             # normalize relative path
             if k in ["documentation"]:
@@ -97,8 +108,11 @@ for tp in ["models", "datasets", "notebooks"]:
         apps = model_info.get("applications")
         if apps is not None and len(apps) > 0:
             for app in apps:
-                assert app in apps_names, "{} not found in the application list, please make sure you use the correct application name.".format(app)
-
+                assert (
+                    app in apps_names
+                ), "{} not found in the application list, please make sure you use the correct application name.".format(
+                    app
+                )
 
         compiled_models.append(model_info)
         compiled_models.sort(key=lambda m: m["name"], reverse=True)
